d4651327c8970b8f4340da7999be2f96f9f6804d
Unknown <nguyentrisinh0810@gmail.com>
2018-05-02 15:35:03 +0700

    Change URL

--- web/hien_portfolio/settings.py
+++ web/hien_portfolio/settings.py
@@ -51,8 +51,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATIC_URL = '/static/'
 STATIC_URL = '/hiennv/static/'
-# STATIC_URL = '/hiennv/static/'
 # MEDIA_URL = '/media/'
 MEDIA_URL = '/hiennv/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
